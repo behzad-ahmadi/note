6f745efbf271a3d25c443f23554f24acc0d7c3b4
Behzad <bh.ahmady@gmail.com>
2024-05-07 01:21:51 +0330

    added env

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -14,13 +14,13 @@ import {
 } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDVCd-O9WKs_ms1CBE36JIrEHs4VNr4ZnA',
-  authDomain: 'mi-noteapp.firebaseapp.com',
-  projectId: 'mi-noteapp',
-  storageBucket: 'mi-noteapp.appspot.com',
-  messagingSenderId: '745572984398',
-  appId: '1:745572984398:web:01aaa2a9daf30a22f4851f',
-  measurementId: 'G-E9YL2LC17G',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
