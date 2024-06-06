81912dc07ccd0d57cba2f99ab164635859e94e48
Behzad Haji ahmadi <bh.ahmady@gmail.com>
2024-06-06 15:43:05 +0330

    test notif

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Notes from '@/app/notes/page'
+import Notif from '@/app/ui/notif'
 
 export default function Home() {
   return (
     <div>
       <Notes />
+      <Notif />
     </div>
   )
 }
---
+++ app/ui/notif.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function Notif() {
+  useEffect(() => {
+    const handleShowNotification = () => {
+      const title = 'Hello!'
+      const options = {
+        body: 'This is a notification from your Next.js app.',
+        icon: '/icon.png', // Optional: specify an icon
+      }
+      showNotification(title, options)
+    }
+    handleShowNotification()
+  }, [])
+
+  return <></>
+}
+
+export const showNotification = (title: string, options: any) => {
+  // Check if the browser supports notifications
+  if (!('Notification' in window)) {
+    alert('This browser does not support desktop notification')
+    return
+  }
+
+  // Check if notification permissions have already been granted
+  if (Notification.permission === 'granted') {
+    new Notification(title, options)
+  }
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission().then(permission => {
+      if (permission === 'granted') {
+        new Notification(title, options)
+      }
+    })
+  }
+}
