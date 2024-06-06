9dc927fc300e09abb9fe13bbbd1f67dc7b4e9589
Behzad <bh.ahmady@gmail.com>
2024-06-06 20:17:30 +0330

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-// import Notes from '@/app/notes/page'
-// import Notif from '@/app/ui/notif'
+import Notes from '@/app/notes/page'
+import Notif from '@/app/ui/notif'
 //
 export default function Home() {
   return (
     <div>
-      {/* <Notes /> */}
-      {/* <Notif /> */}
+      <Notes />
+      <Notif />
     </div>
   )
 }
