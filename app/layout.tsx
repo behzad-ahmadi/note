5667a60aa2040dced37912b3036439b3870c4977
Behzad <bh.ahmady@gmail.com>
2024-06-06 19:57:56 +0330

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import MainNavbar from '@/app/ui/mainNavbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- app/ui/textEditor.tsx
+++ app/ui/textEditor.tsx
@@ -1,7 +1,7 @@
-import dynamic from 'next/dynamic'
-import 'react-quill/dist/quill.snow.css'
+// import dynamic from 'next/dynamic'
+// import 'react-quill/dist/quill.snow.css'
 
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false })
+// const ReactQuill = dynamic(() => import('react-quill'), { ssr: false })
 
 interface TextEditorProps {
   className?: string
